80a4ba10c6e32646619a15c8465cdb3ccc7e9bd0
poupiTanques <138733846+poupiTanques@users.noreply.github.com>
2024-05-14 21:44:48 -0300

    Usar metodos set para superHeroe

--- src/main/java/com/github/poupitanques/Main.java
+++ src/main/java/com/github/poupitanques/Main.java
@@ -9,11 +9,18 @@ import com.github.poupitanques.proyecto.SuperHeroe;
 
 public class Main {
     public static void main(String[] args) {
-        SuperHeroe superHeroe1 = new SuperHeroe("LA VENGANZA (batman)", "el murcielago de la noche vengozo", true);
+        SuperHeroe superHeroe1 = new SuperHeroe("LA VENGANZA (batman)");
+        superHeroe1.setDescripcion("el murcielago de la noche vengozo");
+        superHeroe1.setTieneCapa(true);
+
         Dimensiones dimensiones1 = new Dimensiones(2.0, 3.0, 1.5);
         Figura figura1 = new Figura("DC48234", 700.0, superHeroe1, dimensiones1);
 
-        SuperHeroe superHeroe2 = new SuperHeroe("EL HOMBRE DE ACERO (superman)", "el alien de kripton", true);
+
+        SuperHeroe superHeroe2 = new SuperHeroe("EL HOMBRE DE ACERO (superman)");
+        superHeroe2.setDescripcion("el alien de kripton");
+        superHeroe2.setTieneCapa(true);
+
         Dimensiones dimensiones2 = new Dimensiones(2.5, 2.1, 1.4);
         Figura figura2 = new Figura("DC12398", 850.0, superHeroe1, dimensiones1);
 
